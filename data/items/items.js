d5d6bb7fc78ae8a1c51ba1316e4c15b51f8e1f90
ineocases <zumagamingarg@gmail.com>
2026-08-07 09:31:40 -0300

    Create items.js

---
+++ data/items/items.js
@@ -0,0 +1,193 @@
+const items = [
+
+{
+id:"government_pc",
+name:"PC del Gobierno",
+slot:"pc",
+price:0,
+tier:0,
+owned:true,
+stats:{
+editing:2,
+render:1,
+quality:0
+},
+unlock:[
+"Videos Cortos"
+],
+description:"La computadora con la que todos empezamos."
+},
+
+{
+id:"office_keyboard",
+name:"Teclado Genérico",
+slot:"keyboard",
+price:0,
+tier:0,
+owned:true,
+stats:{},
+unlock:[]
+},
+
+{
+id:"old_phone",
+name:"Celular Viejo",
+slot:"camera",
+price:0,
+tier:0,
+owned:true,
+stats:{
+quality:3
+},
+unlock:[
+"Grabar Videos"
+]
+},
+
+{
+id:"headphones_mic",
+name:"Auriculares con Micrófono",
+slot:"microphone",
+price:0,
+tier:0,
+owned:true,
+stats:{
+audio:2
+},
+unlock:[]
+},
+
+{
+id:"plastic_chair",
+name:"Silla Plástica",
+slot:"chair",
+price:0,
+tier:0,
+owned:true,
+stats:{}
+},
+
+{
+id:"cheap_webcam",
+name:"Webcam HD",
+slot:"camera",
+price:1800,
+tier:1,
+stats:{
+quality:6
+},
+unlock:[
+"Facecam"
+]
+},
+
+{
+id:"fifine",
+name:"FIFINE A6",
+slot:"microphone",
+price:2600,
+tier:1,
+stats:{
+audio:8
+}
+},
+
+{
+id:"ring_light",
+name:"Aro de Luz",
+slot:"light",
+price:1500,
+tier:1,
+stats:{
+quality:4
+}
+},
+
+{
+id:"basic_pc",
+name:"PC Gamer Básica",
+slot:"pc",
+price:9000,
+tier:1,
+stats:{
+editing:12,
+render:10
+},
+unlock:[
+"Videos Largos"
+]
+},
+
+{
+id:"blue_yeti",
+name:"Blue Yeti",
+slot:"microphone",
+price:9000,
+tier:2,
+stats:{
+audio:16
+}
+},
+
+{
+id:"sony_zve10",
+name:"Sony ZV-E10",
+slot:"camera",
+price:22000,
+tier:2,
+stats:{
+quality:18
+},
+unlock:[
+"Vlogs",
+"IRL"
+]
+},
+
+{
+id:"gaming_pc",
+name:"PC Gamer Avanzada",
+slot:"pc",
+price:45000,
+tier:2,
+stats:{
+editing:24,
+render:25
+},
+unlock:[
+"Streaming",
+"4K"
+]
+},
+
+{
+id:"shure",
+name:"Shure SM7B",
+slot:"microphone",
+price:40000,
+tier:3,
+stats:{
+audio:30
+},
+unlock:[
+"Podcast"
+]
+},
+
+{
+id:"sony_a7iv",
+name:"Sony A7 IV",
+slot:"camera",
+price:98000,
+tier:3,
+stats:{
+quality:35
+},
+unlock:[
+"Cinemático"
+]
+}
+
+];
+
+export default items;
