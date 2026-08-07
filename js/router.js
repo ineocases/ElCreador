3d1461c6d0212e247a91f34e255ccba557b20df1
ineocases <zumagamingarg@gmail.com>
2026-08-07 06:07:45 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -2,13 +2,17 @@ import { gameState } from '../engine/gameState.js';
 import { renderCreateChannel } from '../screens/createChannel.js';
 import { renderDashboard } from '../screens/dashboard.js';
 import { renderPublishVideo } from '../screens/publishVideo.js';
+import { renderPasanCosas } from '../screens/pasanCosas.js';
 import { renderVideoResult } from '../screens/videoResult.js';
+import { renderPretemporada } from '../screens/pretemporada.js';
 
 const routes = {
   '#createChannel': renderCreateChannel,
   '#dashboard': renderDashboard,
   '#publish': renderPublishVideo,
-  '#videoResult': renderVideoResult
+  '#pasanCosas': renderPasanCosas,
+  '#videoResult': renderVideoResult,
+  '#pretemporada': renderPretemporada
 };
 
 export function initRouter() {
