aa593aa10f5e9c0ab56139e90265c1caf2f49db4
ineocases <zumagamingarg@gmail.com>
2026-08-11 16:33:41 -0300

    Delete js directory

--- js/router.js
+++
@@ -1,6 +0,0 @@
-// Router reservado para futuras pantallas.
-export class Router {
-    constructor(){this.routes={}}
-    add(path,handler){this.routes[path]=handler;return this}
-    navigate(path){if(this.routes[path])this.routes[path]()}
-}
